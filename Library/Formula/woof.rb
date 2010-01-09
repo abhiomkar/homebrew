15f56c3b4d03bb6b4c4c114d4fbc6694cb4ea769
Abhinay Omkar <abhiomkar@gmail.com>
2010-01-10 00:51:08 +0530

    revert back the woof formula to original

--- Library/Formula/woof.rb
+++ Library/Formula/woof.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Woof <ScriptFileFormula
-  version '20091227'
+  version '20090227'
   url "http://www.home.unix-ag.org/simon/woof"
-  md5 '4df770eedef7b011fc37d42015c801b9'
+  md5 '87d71978789e3444c115048dd686d418'
   homepage 'http://www.home.unix-ag.org/simon/woof.html'
 end
