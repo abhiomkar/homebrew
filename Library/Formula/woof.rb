164153e6b3351f0b4ad2917011ccf9e89256208f
Abhinay Omkar <abhiomkar@gmail.com>
2010-01-09 18:36:37 +0530

    corrected md5sum in woof formula for latest version

--- Library/Formula/woof.rb
+++ Library/Formula/woof.rb
@@ -3,6 +3,6 @@ require 'formula'
 class Woof <ScriptFileFormula
   version '20090227'
   url "http://www.home.unix-ag.org/simon/woof"
-  md5 '87d71978789e3444c115048dd686d418'
+  md5 '4df770eedef7b011fc37d42015c801b9'
   homepage 'http://www.home.unix-ag.org/simon/woof.html'
 end
